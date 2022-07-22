56f8ea141aba9b10b0c6dbf1fac73da1d81c61d4
Manish <manishgowd09@gmail.com>
2022-07-22 20:29:42 +0530

    switch to named pipes

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # fclip
-CLI form of GUI file manager's copy, cut and paste operations  
+### CLI form of GUI file manager's copy, cut and paste operations  
 
-GUI Clipboard emulation in command line
+### GUI Clipboard emulation in command line
 
-Approach:
+
+__Approach__:
 * Select a group of items for copying or moving
 * We select that group of items in a particular directory
 * So, we take note of the directory where we selected items for copying/moving
@@ -14,3 +15,17 @@ Approach:
 * So for the above reasons, we need to keep the list of items persistent, since we could select them for copy/move at some instant of time and paste them in another directory at some other instant of time, which could be a lot later than the timestamp at which the items had been selected
 * So we write the above 3 details (operation, source directory, list of items) to a file, and then refer the file for pasting
 * The file used for the above purpose is known as the clip file
+* But reading from and writing to file then mostly involves a disk operation which we obviously want to avoid, else it defeats the whole purpose of copying to buffer
+* So rather than using a file, we use named pipes (which are used for communication between 2 processes)
+* Named pipes are much faster since the interprocess communication happens in the memory itself
+
+
+__Prerequisites__:
+* [fd](https://github.com/sharkdp/fd) (better version of find)
+
+
+__Commands__:
+```
+git clone https://github.com/Manish0925/fclip
+echo "source $PWD/fclip/fclip.zsh" > $HOME/.zshrc
+```
--- fclip.zsh
+++ fclip.zsh
@@ -1,27 +1,52 @@
-# check if $CLIP_FILE is defined
-if [[ ! $+$CLIP_FILE ]]
-then
-  export CLIP_FILE="$HOME/.clip_dir/clip_file.txt"
+# NOTE: we use $ before the subshell if we want to store the result for future use
+# else just using subshell without '$' symbol is enough
+
+
+# check if $CLIP_FILE is exported
+# if not source it to zsh config file
+if [[ -z $CLIP_FILE ]]; then
+  echo 'export CLIP_FILE=/tmp/clip_file' > $HOME/.zshenv
+  export CLIP_FILE=/tmp/clip_file
 fi
 
-# check if $CLIP_FILE exists
-if [[ ! -f $CLIP_FILE ]]
-then
-  echo "Clip file not found\nCreating it..."
-  mkdir -p $HOME/.clip_dir
-  touch $HOME/.clip_dir/clip_file.txt
-fi
+# -----------------------------------------------------------------------------------------------------
+# ASSIST FUNCTIONS
+# -----------------------------------------------------------------------------------------------------
+# function to refresh clipboard with check
+function refresh_clipboard_with_check() {
+  # NOTE: named pipes never get overwritten, so remove them if they exist, and create them again
+  # we do this in order to prevent appending
+  [[ -p $CLIP_FILE ]] && rm $CLIP_FILE
+  mkfifo $CLIP_FILE
+}
+
+
+# function to refresh clipboard without check
+function refresh_clipboard_without_check() {
+  # NOTE: named pipes never get overwritten, so remove them if they exist, and create them again
+  # we do this in order to prevent appending
+  rm $CLIP_FILE
+  mkfifo $CLIP_FILE
+}
+
+
+# function to clear clipboard
+function clear_clipboard() {
+  rm $CLIP_FILE
+}
+# -----------------------------------------------------------------------------------------------------
 
 
 # -----------------------------------------------------------------------------------------------------
-# FUNCTIONS
+# DIRECT FUNCTIONS
 # -----------------------------------------------------------------------------------------------------
-
 # function to specify action as "copy" and store contents' addresses in clip_file
 function fcp() {
   [[ $# -eq 0 ]] && return
   items_list=$(printf '%s\n' $@)
-  echo "cp\n$PWD\n$items_list" > $CLIP_FILE
+  refresh_clipboard_with_check
+  # convert statement to subshell in order to prevent echoing of background output
+  ( echo "cp\n$PWD\n$items_list" > $CLIP_FILE & )
 }
 # tab completion function for fcp
 function _fcp() {
@@ -37,7 +62,9 @@ compdef _fcp fcp
 function fmv() {
   [[ $# -eq 0 ]] && return
   items_list=$(printf '%s\n' $@)
-  echo "mv\n$PWD\n$items_list" > $CLIP_FILE
+  refresh_clipboard_with_check
+  # convert statement to subshell in order to prevent echoing of background output
+  ( echo "mv\n$PWD\n$items_list" > $CLIP_FILE & )
 }
 # tab completion function for fmv
 function _fmv() {
@@ -49,21 +76,28 @@ function _fmv() {
 compdef _fmv fmv
 
 
+# NOTE: for copy operation, we store previously contents in clipboard, since their location remains the same
+# NOTE: for move operation, we don't store previously contents in clipboard, since their location has changed
+
 # function to paste clip_file contents
 function fp() {
-  if [[ ! -s $CLIP_FILE ]]; then
+  if [[ ! -p $CLIP_FILE ]]; then
     echo 'Nothing to show'
     return
   fi
-  local clip_file_details=(${(@f)$(cat $CLIP_FILE)})
-  local clip_file_elements=(${clip_file_details:2})
-  clip_file_elements=(${clip_file_elements[@]/#/$clip_file_details[2]/})
-  if [[ $clip_file_details[1] = 'cp' ]]; then
-    clip_file_details[1]+=' -r'
+  local clip_file_details=$(cat $CLIP_FILE)
+  local clip_file_details_array=(${(@f)clip_file_details})
+  local clip_file_elements=(${clip_file_details_array:2})
+  clip_file_elements=(${clip_file_elements[@]/#/$clip_file_details_array[2]/})
+  if [[ $clip_file_details_array[1] = 'cp' ]]; then
+    cp --recursive $clip_file_elements[@] .
+    refresh_clipboard_without_check
+    # convert statement to subshell in order to prevent echoing of background output
+    ( echo $clip_file_details > $CLIP_FILE & )
   else
-    truncate -s 0 $CLIP_FILE
+    mv $clip_file_elements[@] .
+    clear_clipboard
   fi
-  eval $clip_file_details[1] $clip_file_elements .
 }
 # tab completion function for fp
 function _fp() {
@@ -74,13 +108,16 @@ compdef _fp fp
 
 # function to display clip file
 function fls() {
-  if [[ ! -s $CLIP_FILE ]]; then
+  if [[ ! -p $CLIP_FILE ]]; then
     echo 'Nothing to show'
     return
   fi
-  local clip_file_details=("${(@f)$(cat $CLIP_FILE)}")
-  printf "Operation\n----------------\n%s\n\nSource Directory\n----------------\n%s\n\nItems\n----------------\n" ${clip_file_details:0:2}
-  printf "%s\n" ${clip_file_details:2}
+  local clip_file_details=$(cat $CLIP_FILE)
+  refresh_clipboard_without_check
+  local clip_file_details_array=("${(@f)clip_file_details}")
+  printf 'Operation\n----------------\n%s\n\nSource Directory\n----------------\n%s\n\nItems\n----------------\n' ${clip_file_details_array:0:2}
+  printf '%s\n' ${clip_file_details_array:2}
+  ( echo $clip_file_details > $CLIP_FILE & )
 }
 # tab completion function for fls
 function _fls() {
@@ -91,15 +128,16 @@ compdef _fls fls
 
 # function to clear clip file
 function fclear() {
-  if [[ ! -s $CLIP_FILE ]]; then
-    echo 'Clip file already empty'
+  if [[ ! -p $CLIP_FILE ]]; then
+    echo 'Nothing in clipboard'
     return
   fi
-  truncate --size 0 $CLIP_FILE
-  echo 'Clip file cleared'
+  clear_clipboard
+  echo 'Clipboard cleared'
 }
 # tab completion function for fclear
 function _fclear() {
   return
 }
 compdef _fclear fclear
+# -----------------------------------------------------------------------------------------------------
